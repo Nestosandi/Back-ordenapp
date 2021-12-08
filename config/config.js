4a6179783a02cd36461a4098636baa3a7df60479
CamiloRodriguezG <72152781+CamiloRodriguezG@users.noreply.github.com>
2021-12-07 22:15:00 -0500

    db lista

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 // module.exports.URI_DB = "mongodb+srv://John9135:0123456789@cluster0.i8uoq.mongodb.net/db_ordenapp?retryWrites=true&w=majority"
-module.exports.URI_DB = "mongodb://localhost:27017/db_ordenapp"
+// module.exports.URI_DB = "mongodb://localhost:27017/db_ordenapp"
+module.exports.URI_DB = "mongodb+srv://grupo54:orappcontraseña@cluster0.ahmss.mongodb.net/orapp?retryWrites=true&w=majority"
 module.exports.NODE_ENV = "development";
 module.exports.PORT = 2021; 
